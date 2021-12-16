87a49accb314491d5ff69132c15406962b20dfce
taisa <g5.taisa831@gmail.com>
2021-12-16 16:15:21 +0900

    chapter02

---
+++ apps/app.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+from apps.config import config
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
+
+
+# create_app関数を作成する
+def create_app(config_key):
+    # Flaskインスタンス生成
+    app = Flask(__name__)
+    app.config.from_object(config[config_key])
+
+    # SQLAlchemyとアプリを連携する
+    db.init_app(app)
+    # Migrateとアプリを連携する
+    Migrate(app, db)
+    csrf.init_app(app)
+
+    # crudパッケージからviewsをimportする
+    from apps.crud import views as crud_views
+
+    # register_blueprintを使いviewsのcrudをアプリへ登録する
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    return app
---
+++ apps/config.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+basedir = Path(__file__).parent.parent
+
+
+# BaseConfigクラスを作成する
+class BaseConfig:
+    SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ"
+    WTF_CSRF_SECRET_KEY = "AuwzyszU5sugKN7KZs6f"
+
+
+# BaseConfigクラスを継承してLocalConfigクラスを作成する
+class LocalConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir / 'local.sqlite'}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+
+
+# BaseConfigクラスを継承してTestingConfigクラスを作成する
+class TestingConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir / 'testing.sqlite'}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WTF_CSRF_ENABLED = False
+
+
+# config辞書にマッピングする
+config = {
+    "testing": TestingConfig,
+    "local": LocalConfig,
+}
