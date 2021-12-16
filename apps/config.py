dfd087325a0d1a7a30502bd52ff6df7ea6d1b99f
taisa <g5.taisa831@gmail.com>
2021-12-16 21:03:04 +0900

    chapter07

--- apps/config.py
+++ apps/config.py
@@ -7,6 +7,8 @@ basedir = Path(__file__).parent.parent
 class BaseConfig:
     SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ"
     WTF_CSRF_SECRET_KEY = "AuwzyszU5sugKN7KZs6f"
+    # 画像アップロード先にapps/imagesを指定する
+    UPLOAD_FOLDER = str(Path(basedir, "apps", "images"))
 
 
 # BaseConfigクラスを継承してLocalConfigクラスを作成する
