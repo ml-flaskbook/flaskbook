49464c202bad7eee9006558f48ef7c503173931f
taisa <g5.taisa831@gmail.com>
2021-12-16 20:53:15 +0900

    chapter06

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -41,7 +41,7 @@ def signup():
         # GETパラメータにnextキーが存在し、値がない場合はユーザーの一覧ページへリダイレクトする
         next_ = request.args.get("next")
         if next_ is None or not next_.startswith("/"):
-            next_ = url_for("crud.users")
+            next_ = url_for("detector.index")
         return redirect(next_)
 
     return render_template("auth/signup.html", form=form)
@@ -58,7 +58,7 @@ def login():
         # ユーザーが存在しパスワードが一致する場合はログインを許可する
         if user is not None and user.verify_password(form.password.data):
             login_user(user)
-            return redirect(url_for("crud.users"))
+            return redirect(url_for("detector.index"))
 
         # ログイン失敗メッセージを設定する
         flash("メールアドレスかパスワードか不正です")
